b76ef9652db55962d6b6b8727f2403eb1c13df42
unknown <you@example.com>
2023-10-23 19:54:44 +0530

    started api

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+
 class SignInForm(forms.Form):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+class ChannelForm(forms.Form):
+    channel_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+  
+class ServiceCategoryMasterForm(forms.Form):
+    service_category_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+  
