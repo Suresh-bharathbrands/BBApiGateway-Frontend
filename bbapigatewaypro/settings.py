31f119960239efa8358a6b6042138de82711931b
unknown <you@example.com>
2023-10-30 13:50:51 +0530

    add some i dont know

--- bbapigatewaypro/settings.py
+++ bbapigatewaypro/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent   
 TEMPLATE_DIR=BASE_DIR/'templates'
 STATIC_DIR=BASE_DIR/'static'
 MEDIA_ROOT=BASE_DIR/'media'
